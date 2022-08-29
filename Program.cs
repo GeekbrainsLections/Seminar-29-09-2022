78c09abfbe149ee82b44aa0d3d728edba92bb7e4
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2022-08-29 20:17:54 +0300

    Matrixes functions added

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int[,] CreateMatrix(int length, int width, int minimum, int maximum)
+{
+    int[,] matrix = new int[length, width];
+    Random random = new Random();
+    for(int i=0;i<matrix.GetLength(0);i++)
+        for(int j=0;j<matrix.GetLength(1);j++)
+            matrix[i,j] = random.Next(minimum,maximum);
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i=0;i<matrix.GetLength(0);i++)
+    {
+        for(int j=0;j<matrix.GetLength(1);j++)
+            Console.Write($"{matrix[i,j]} ");
+        Console.WriteLine();
+    }
+
+}
+Console.Write("Enter amount of rows in matrix: ");
+int rows = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter amount of columns in matrix: ");
+int columns = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = CreateMatrix(rows,columns,0,30);
+PrintMatrix(matrix);
